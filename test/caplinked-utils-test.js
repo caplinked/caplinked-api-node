dc53835577f470ff20ec54dc520468f752f27ba1
Arons Lee <arons@caplinked.com>
2016-11-30 15:52:38 -0800

    Test for utils

---
+++ test/caplinked-utils-test.js
@@ -0,0 +1,27 @@
+/* eslint-env mocha */
+
+var chai = require('chai');
+var sinon = require('sinon');
+var assert = chai.assert;
+
+var clUtils = require('../src/caplinked-utils');
+
+describe('CaplinkedUtils', function () {
+
+  var cl;
+
+  describe('path helper', function () {
+    it('can return path uri with no params', function () {
+      var uri = clUtils.path('/api/v1/test');
+      assert.equal(uri, '/api/v1/test');
+    });
+    it('can return path uri with params', function () {
+      var uri = clUtils.path('/api/v1/test/:param1/:param2/yee', {
+        ':param1': 'abc',
+        ':param2': 'def'
+      });
+      assert.equal(uri, '/api/v1/test/abc/def/yee');
+    });
+  });
+
+});
