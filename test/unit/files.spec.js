24b660cb2a05a52149273318d06d40db11532f7e
William Carron <wcarron27@users.noreply.github.com>
2018-09-07 16:58:52 -0700

    Fix function argument bug in files.js - missing variable due to missp… (#8)
    * Fix function argument bug in files.js - missing variable due to misspelling. Add unit test for getFileInfo

---
+++ test/unit/files.spec.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+var chai = require('chai')
+var sinon = require('sinon')
+var assert = chai.assert
+
+var CapLinkedSDK = require('./../../src/caplinked-sdk')
+var Files = require('./../../src/resources/files.js')
+var httpRequest = require('./../../src/http-request')
+var CL_CONSTANTS = require('./../../src/caplinked-constants')
+
+describe('Files Component', function () {
+  var client = {
+    request: function (method, path, options, params) {
+      return { title: 'File title', meta: true }
+    }
+  };
+
+  describe('getFileInfo', function () {
+    it('Returns file information when passed proper ids', function () {
+      f = new Files(client)
+      assert.deepEqual(f.getFileInfo(123, 132, 123), { title: 'File title', meta: true })
+    });
+  });
+});
